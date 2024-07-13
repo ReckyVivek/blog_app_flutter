65d358adbc860d4ee42c23735806de75fc16bffa
Vivek Kumar Dhoke <ravishankar20041973@gmail.com>
2024-07-13 11:56:59 +0530

    Blog page UI done

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -10,8 +10,12 @@ class AppTheme {
       borderRadius: BorderRadius.circular(10));
   static final darkThemeMode = ThemeData.dark().copyWith(
     scaffoldBackgroundColor: AppPallete.backgroundColor,
-    appBarTheme: const AppBarTheme(
-      backgroundColor: AppPallete.backgroundColor
+    appBarTheme: const AppBarTheme(backgroundColor: AppPallete.backgroundColor),
+    chipTheme: const ChipThemeData(
+      color: MaterialStatePropertyAll(
+        AppPallete.backgroundColor,
+      ),
+      side: BorderSide.none,
     ),
     inputDecorationTheme: InputDecorationTheme(
       contentPadding: const EdgeInsets.all(27),
@@ -19,4 +23,4 @@ class AppTheme {
       focusedBorder: _border(AppPallete.gradient2),
     ),
   );
-} 
+}
